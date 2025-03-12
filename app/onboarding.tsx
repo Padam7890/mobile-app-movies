76586bc836351071392361fbf0082aeb6f60be55
Padam7890 <padamthapamoho@gmail.com>
2025-03-12 19:47:26 +0545

    feat : new begining

---
+++ app/onboarding.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Onboarding = () => {
+  return (
+    <View>
+        <Text className=' px-3 py-4'>Hello</Text>
+    </View>
+  )
+}
+
+export default Onboarding
